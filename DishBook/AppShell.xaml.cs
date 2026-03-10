d08c73c70d61dec35172c5cd21644c0a1c545e0f
Maria Smith <maria.t.smith023@gmail.com>
2026-03-10 10:16:36 -0400

    initial commit

---
+++ DishBook/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DishBook
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DishBook/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DishBook")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DishBook.Pages")]
