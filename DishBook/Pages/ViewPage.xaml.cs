52ac2e915e3a9b45b592ca1ae35fa0d752c9ee07
Maria Smith <maria.t.smith023@gmail.com>
2026-03-20 12:23:02 -0400

    home page structure, components

--- DishBook/Pages/ViewPage.xaml.cs
+++ DishBook/Pages/ViewPage.xaml.cs
@@ -2,8 +2,34 @@ namespace DishBook;
 
 public partial class ViewPage : ContentPage
 {
-	public ViewPage()
-	{
-		InitializeComponent();
-	}
+    // TODO: Implement actual Recipe entity
+    private record DummyRecipe(string Name);
+
+    private readonly List<DummyRecipe> _allRecipes = new()
+    {
+        new DummyRecipe("Spaghetti Carbonara"),
+        new DummyRecipe("Chicken Stir Fry"),
+        new DummyRecipe("Banana Bread"),
+        new DummyRecipe("Caesar Salad"),
+        new DummyRecipe("Beef Tacos"),
+        new DummyRecipe("Tomato Soup"),
+    };
+
+    public ViewPage()
+    {
+        InitializeComponent();
+        RecipesCollection.ItemsSource = _allRecipes;
+    }
+
+    private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+    {
+        // TODO: Search functionality
+    }
+
+    private async void OnRecipeTapped(object sender, TappedEventArgs e)
+    {
+        // TODO: Open recipe page
+        if (e.Parameter is not DummyRecipe recipe) return;
+        await DisplayAlert("Recipe", $"Tapped: {recipe.Name}", "OK");
+    }
 }
